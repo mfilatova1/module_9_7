06b781ca68f24f7fdcb494a68c4893356ba00882
mfilatova1 <filatovam435@gmail.com>
2024-08-29 19:09:15 +0300

    Add files via upload

---
+++ module_9_7.py
@@ -0,0 +1,29 @@
+def is_prime(func):
+    def wrapper(*num:int):
+        result = func(*num)
+        for div in range(2, result):
+            if result % div == 0:
+                print("Составное")
+                return result
+            else:
+                print("Простое")
+                return result
+    return wrapper
+
+
+@is_prime
+def sum_three(a,b,c):
+    return a + b + c
+
+
+
+
+result = sum_three(2, 3, 6)
+print(result)
+
+
+
+
+
+
+
